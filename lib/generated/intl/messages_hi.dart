fda5b4c3c3ae7aad5eedce2326f2e6af1c23161a
akshayrajput <akshayrajput9634@gmail.com>
2025-11-10 00:50:12 +0530

    kafi change h

---
+++ lib/generated/intl/messages_hi.dart
@@ -0,0 +1,106 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a hi locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'hi';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "aboutUs": MessageLookupByLibrary.simpleMessage("हमारे बारे में"),
+    "appLanguage": MessageLookupByLibrary.simpleMessage("ऐप की भाषा"),
+    "appSettings": MessageLookupByLibrary.simpleMessage("ऐप सेटिंग्स"),
+    "appTheme": MessageLookupByLibrary.simpleMessage("ऐप थीम"),
+    "areYouSureYouWantToLogout": MessageLookupByLibrary.simpleMessage(
+      "क्या आप वाकई लॉगआउट करना चाहते हैं?",
+    ),
+    "bookmark": MessageLookupByLibrary.simpleMessage("बुकमार्क"),
+    "byContinuingToUseThisSiteYouAcceptOurUse":
+        MessageLookupByLibrary.simpleMessage(
+          "इस साइट का उपयोग जारी रखकर, आप हमारे कुकीज़ के उपयोग से सहमत हैं ताकि आप लॉग-इन और सुरक्षित रह सकें।",
+        ),
+    "cancel": MessageLookupByLibrary.simpleMessage("रद्द करें"),
+    "categories": MessageLookupByLibrary.simpleMessage("श्रेणियाँ"),
+    "clickToRefresh": MessageLookupByLibrary.simpleMessage(
+      "रीफ़्रेश करने के लिए टैप करें।",
+    ),
+    "continueWithGoogle": MessageLookupByLibrary.simpleMessage(
+      "Google से जारी रखें",
+    ),
+    "dark": MessageLookupByLibrary.simpleMessage("डार्क"),
+    "diffindays30floorMoAgo": MessageLookupByLibrary.simpleMessage(
+      "\${(diff.inDays / 30).floor()} महीने पहले",
+    ),
+    "diffindays365floorYrAgo": MessageLookupByLibrary.simpleMessage(
+      "\${(diff.inDays / 365).floor()} साल पहले",
+    ),
+    "diffindays7floorWAgo": MessageLookupByLibrary.simpleMessage(
+      "\${(diff.inDays / 7).floor()} हफ़्ते पहले",
+    ),
+    "diffindaysDaysAgo": MessageLookupByLibrary.simpleMessage(
+      "\${diff.inDays} दिन पहले",
+    ),
+    "diffinhoursHrAgo": MessageLookupByLibrary.simpleMessage(
+      "\${diff.inHours} घंटे पहले",
+    ),
+    "diffinminutesMinAgo": MessageLookupByLibrary.simpleMessage(
+      "\${diff.inMinutes} मिनट पहले",
+    ),
+    "failedToLoadImage": MessageLookupByLibrary.simpleMessage(
+      "छवि लोड करने में विफल",
+    ),
+    "home": MessageLookupByLibrary.simpleMessage("होम"),
+    "justNow": MessageLookupByLibrary.simpleMessage("अभी-अभी"),
+    "light": MessageLookupByLibrary.simpleMessage("लाइट"),
+    "logout": MessageLookupByLibrary.simpleMessage("लॉगआउट"),
+    "noBookmarksYet": MessageLookupByLibrary.simpleMessage(
+      "अभी तक कोई बुकमार्क नहीं है।",
+    ),
+    "noInternetConnection": MessageLookupByLibrary.simpleMessage(
+      "कोई इंटरनेट कनेक्शन नहीं है",
+    ),
+    "noNewsAvailable": MessageLookupByLibrary.simpleMessage(
+      "कोई समाचार उपलब्ध नहीं है",
+    ),
+    "noNewsFound": MessageLookupByLibrary.simpleMessage(
+      "कोई समाचार नहीं मिला।",
+    ),
+    "notavailable": MessageLookupByLibrary.simpleMessage("उपलब्ध नहीं"),
+    "other": MessageLookupByLibrary.simpleMessage("अन्य"),
+    "profile": MessageLookupByLibrary.simpleMessage("प्रोफ़ाइल"),
+    "rateUs": MessageLookupByLibrary.simpleMessage("हमें रेट करें"),
+    "realFactsNoBiasGlobalUpdatesnYouCanTrust":
+        MessageLookupByLibrary.simpleMessage(
+          "सच्ची ख़बरें। बिना पक्षपात के। विश्व की वो अपडेट जिन पर आप भरोसा कर सकते हैं।",
+        ),
+    "searchNews": MessageLookupByLibrary.simpleMessage("समाचार खोजें"),
+    "selectLanguage": MessageLookupByLibrary.simpleMessage("भाषा चुनें"),
+    "selectTheme": MessageLookupByLibrary.simpleMessage("थीम चुनें"),
+    "shareApp": MessageLookupByLibrary.simpleMessage("ऐप साझा करें"),
+    "sourceInfoNotAvailable": MessageLookupByLibrary.simpleMessage(
+      "स्रोत जानकारी उपलब्ध नहीं है",
+    ),
+    "systemDefault": MessageLookupByLibrary.simpleMessage("सिस्टम डिफ़ॉल्ट"),
+    "typeSomethingToGetResults": MessageLookupByLibrary.simpleMessage(
+      "परिणाम पाने के लिए कुछ टाइप करें...",
+    ),
+    "unknownsource": MessageLookupByLibrary.simpleMessage("अज्ञात स्रोत"),
+    "viewFullArticle": MessageLookupByLibrary.simpleMessage("पूरा लेख देखें"),
+    "yesterday": MessageLookupByLibrary.simpleMessage("कल"),
+  };
+}
